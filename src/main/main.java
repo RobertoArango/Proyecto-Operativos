717f0ac40960c622a102eef0b6218c25d99d79ba
RobertoArango <roberto.arango@correo.unimet.edu.ve>
2023-05-23 12:10:54 -0400

    variables listas

--- src/main/main.java
+++ src/main/main.java
@@ -10,6 +10,31 @@ package main;
  * @author rober
  */
 public class main {
+    //los valores lo colocamos temporalmente como placeholder
+    //la "a" indica que estan en almacen 
+    public static volatile int aChasis = 1; 
+    public static volatile int aCarrocerias = 1;
+    public static volatile int aMotores = 1;
+    public static volatile int aRuedas = 5;
+    public static volatile int aAccesorios = 1;
+    public static volatile boolean tGerente = true;
+    public static volatile boolean tDirector = true;
+    
+     
+    public static volatile int carnet;
+    public static volatile int chasis;
+    public static volatile int carrocerias;
+    public static volatile int motores;
+    public static volatile int ruedas;
+    public static volatile int accesorios;
+    public static volatile int cantChasis;
+    public static volatile int cantMotores;
+    public static volatile int cantRuedas;
+    public static volatile int cantAccesorios;
+    public static volatile float precio;
+    public static volatile int maximoProducto;
+    
+    
 
     /**
      * @param args the command line arguments
