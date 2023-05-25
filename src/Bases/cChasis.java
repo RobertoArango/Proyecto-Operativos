18515d34416d4b86ba14d0a604d98648fb48756b
Victor Llanos <victor.llanos@correo.unimet.edu.ve>
2023-05-25 13:16:31 -0400

    codigo de productor (prueba en chasis) - revisar parametros enviados

--- src/Bases/cChasis.java
+++ src/Bases/cChasis.java
@@ -5,6 +5,9 @@
 package Bases;
 
 import java.util.concurrent.Semaphore;
+import javax.swing.JOptionPane;
+import main.main;
+
 
 /**
  *
@@ -15,5 +18,29 @@ public class cChasis extends Productores {
     public cChasis(float tProduccion, float pDia, float salario, Semaphore mutex, Semaphore semProduccion, Semaphore semSalario) {
         super(pDia, salario, mutex, semProduccion, semSalario);
     }
+    
+    @Override
+    public void run() {
+        try {
+            while (this.semProduccion.availablePermits() == 0) {
+                Thread.sleep((long) (1* 1000 / 24));
+                this.semSalario.acquire();
+                salario += 10;
+                this.semSalario.release();
+            }
+            Thread.sleep((long) this.tProduccion);
+            this.semProduccion.acquire();
+            this.mutex.acquire();
+            this.semSalario.acquire();
+            salario += ((tProduccion/1000)*10);
+            main.aChasis++;
+            //System.out.println("Chasis:" + main.aChasis);
+            this.mutex.release();
+            this.semSalario.release();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "error", "ERROR", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+   }
     
 }
